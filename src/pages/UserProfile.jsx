ca98d7d34cb84acf1765c8dbdf56b0bab364038c
Michelle <michellecheng.2017@gamil.com>
2022-04-07 22:49:41 -0400

    implemented profile

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 
-function UserProfile() {
+function UserProfile({ clientInfo }) {
+  console.log(clientInfo);
   return (
     <div>
       <Navbar />
+      <div style={{ textAlign: "center" }}>
+        <img
+          src="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+          alt="profile-img"
+        />
+        <h1>User Profile</h1>
+        <h2>User: {clientInfo.currentUser.username}</h2>
+        <h2>Member Since: {clientInfo.currentUser.memberSince}</h2>
+      </div>
     </div>
   );
 }
